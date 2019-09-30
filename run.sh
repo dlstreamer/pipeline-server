98aa6033f254b6d7daedffdfffc872c5fd40488e
Tobias Mosby <tobias.mosby@intel.com>
2019-09-30 11:05:54 -0700

    Consistent indent, add corresponding shell script to stop containers.

--- run.sh
+++ run.sh
@@ -1,18 +1,17 @@
 #!/bin/bash -e
 run_gstreamer () {
-sudo docker run -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 8080:8080 -v /tmp:/tmp --rm video_analytics_serving_gstreamer:latest
+  sudo docker run -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 8080:8080 -v /tmp:/tmp --name video_analytics_serving_gstreamer --rm video_analytics_serving_gstreamer:latest
 }
 run_ffmpeg () {
-sudo docker run -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 8080:8080 -v /tmp:/tmp --rm video_analytics_serving_ffmpeg:latest
+  sudo docker run -e http_proxy=$http_proxy -e https_proxy=$https_proxy -p 8080:8080 -v /tmp:/tmp --name video_analytics_serving_ffmpeg --rm video_analytics_serving_ffmpeg:latest
 }
-if [ $# -eq 0 ]
-  then
+if [ $# -eq 0 ]; then
   run_gstreamer
 else
   build="$1"
   if [ "$build" = "ffmpeg" ]; then
-     run_ffmpeg
+    run_ffmpeg
   else
-     run_gstreamer
+    run_gstreamer
   fi
 fi
