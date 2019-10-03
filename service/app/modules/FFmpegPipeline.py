142bb64a0439bc1834a6d1401099b621fff2a7e8
Thanaji Rao Thakkalapelli <thanaji.rao.thakkalapelli@intel.com>
2019-10-03 12:01:42 -0700

    fix errors from tests

--- service/app/modules/FFmpegPipeline.py
+++ service/app/modules/FFmpegPipeline.py
@@ -53,7 +53,8 @@ class FFmpegPipeline(Pipeline):
 
     def params(self):
         request = copy.deepcopy(self.request)
-        del(request["models"])
+        if "models" in request:
+            del(request["models"])
 
         params_obj = {
             "id": self.id,
