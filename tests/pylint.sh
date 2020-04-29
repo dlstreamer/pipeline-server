1b342051b85682beadb1ec173f848697fa2f02b2
Tobias Mosby <tobias.mosby@intel.com>
2020-04-29 16:32:48 -0700

    remove tests from RC
    Signed-off-by: Tobias Mosby <tobias.mosby@intel.com>

--- tests/pylint.sh
+++
@@ -1,6 +0,0 @@
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-SOURCE_DIR=$(dirname $SCRIPT_DIR)
-
-rm -rf pylint.results.json
-python3 -m pylint ${SOURCE_DIR}/**/*.py --reports=y --output-format=json --score=y  --exit-zero >>pylint.results.json
--- tests/pytest.sh
+++
@@ -1,4 +0,0 @@
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-SOURCE_DIR=$(dirname $SCRIPT_DIR)
-
-cd ${SOURCE_DIR}; python3 -m pytest -s
