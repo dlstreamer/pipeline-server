343e0ad7a96240b60652fb0e0debed137f4a94ef
nnshah1 <neelay.n.shah@intel.com>
2020-05-14 13:37:51 -0700

    Modifications to sample and documentation

--- samples/integration/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# Building Video Analytics Serving as a base image. GStreamer integration example.
-VIDEO_ANALYTICS_SERVING_REPO="https://github.intel.com/intel/video-analytics-serving.git"
-sudo docker build ${VIDEO_ANALYTICS_SERVING_REPO} --file Dockerfile.gst -t "video_analytics_serving_gstreamer_base:latest" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /')
-
-# Next integrate your service/application to be deployed and run alongside Video Analytics Serving components.
-# To build your docker file, check "SampleIntegrationDockerfile"
-sudo docker build -t "video_analytics_serving_gstreamer:latest" $(env | grep -E '_(proxy|REPO|VER)=' | sed 's/^/--build-arg /') .
