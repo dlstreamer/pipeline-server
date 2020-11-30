145edf65edef6dc261b9aa2c543c53c2cb190540
Henry Bruce <henry.bruce@intel.com>
2020-11-30 15:39:04 -0800

    Remove tests

--- samples/lva_ai_extension/tests/run.sh
+++
@@ -1,6 +0,0 @@
-IMAGE=video-analytics-serving-lva-tests
-CURRENT_DIR=$(dirname $(readlink -f "$0"))
-ROOT_DIR=$(readlink -f "$CURRENT_DIR/../../..")
-LVA_DIR=$(dirname $CURRENT_DIR)
-docker stop $IMAGE
-"$ROOT_DIR/docker/run.sh" --image $IMAGE -v /dev/shm:/dev/shm --pipelines "$LVA_DIR/pipelines" -p 5001:5001 --user $UID "$@"
