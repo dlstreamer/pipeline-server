438f6424707f7783638580cf20026383634734c5
Thakkalapelli, Thanaji Rao <thanaji.rao.thakkalapelli@intel.com>
2021-01-06 16:02:42 -0800

    Added html report for LVA tests and updated Teamcity to attach artifacts

--- samples/lva_ai_extension/tests/run.sh
+++ samples/lva_ai_extension/tests/run.sh
@@ -3,4 +3,8 @@ CURRENT_DIR=$(dirname $(readlink -f "$0"))
 ROOT_DIR=$(readlink -f "$CURRENT_DIR/../../..")
 LVA_DIR=$(dirname $CURRENT_DIR)
 docker stop $IMAGE
-"$ROOT_DIR/docker/run.sh" --image $IMAGE -v /dev/shm:/dev/shm --pipelines "$LVA_DIR/pipelines" -p 5001:5001 --user $UID "$@"
+LOCAL_RESULTS_DIR="$CURRENT_DIR/results"
+DOCKER_RESULTS_DIR="/home/video-analytics-serving/samples/lva_ai_extension/tests/results"
+mkdir -p "$LOCAL_RESULTS_DIR"
+RESULTS_VOLUME_MOUNT="-v $LOCAL_RESULTS_DIR:$DOCKER_RESULTS_DIR "
+"$ROOT_DIR/docker/run.sh" --image $IMAGE -v /dev/shm:/dev/shm $RESULTS_VOLUME_MOUNT --pipelines "$LVA_DIR/pipelines" -p 5001:5001 --user $UID "$@"
