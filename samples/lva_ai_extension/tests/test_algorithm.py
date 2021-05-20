82dd66a156a6a04c233f54b26de87aeb83fc45c5
Bruce, Henry <henry.bruce@intel.com>
2021-05-19 21:27:33 -0700

    Add vehicle zone event pipeline

--- samples/lva_ai_extension/tests/test_algorithm.py
+++ samples/lva_ai_extension/tests/test_algorithm.py
@@ -9,6 +9,7 @@ import cv2
 def count_events(results, event_type, valid_event_names):
     print("Counting events of type {} with a name in {}".format(event_type, valid_event_names))
     num_events = 0
+    total_zone_count = 0
     with open(results, "r") as file_:
         for line in file_:
             if line and line != '':
@@ -18,8 +19,11 @@ def count_events(results, event_type, valid_event_names):
                         if inference['type'] == 'EVENT' and inference['subtype'] == event_type:
                             if inference['event']['name'] in valid_event_names:
                                 num_events+=1
+                                if event_type == "zoneCrossing":
+                                    key = "zoneCount"
+                                    total_zone_count += int(inference['event']['properties'][key])
                                 print(inference)
-    return num_events
+    return num_events, total_zone_count
 
 def get_event_names(client_params):
     event_parameter_key = client_params["event_parameter"]
@@ -50,7 +54,9 @@ def test_algorithm(helpers, test_case, test_filename, generate):
         assert process.wait() == expected_return_code
     helpers.validate_output_against_schema(output_file)
     valid_event_names = get_event_names(client_params)
-    event_count = count_events(output_file, client_params["expected_event_type"], valid_event_names)
-    print("event_count = {}".format(event_count))
+    event_count, total_count = count_events(output_file, client_params["expected_event_type"], valid_event_names)
+    print("event_count = {}, total_count = {}".format(event_count, total_count))
     assert event_count == client_params["expected_event_count"], "Incorrect number of events detected"
+    assert total_count == client_params.get("expected_total_count", 0), "Incorrect total count detected"
+
 
