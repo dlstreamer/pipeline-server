2800e03d7bee9980986370887f477e856d5b3710
Vidya Galli <vidya.s.galli@intel.com>
2022-02-07 12:54:10 -0800

    Record frames update
    * Doc and log message update
    
    * readme only change
    
    * reversing code change
    
    * removed line
    
    * adding back change
    
    * Update README.md
    
    Co-authored-by: Henry Bruce <henry.bruce@intel.com>

--- samples/record_frames/mqtt_client.py
+++ samples/record_frames/mqtt_client.py
@@ -68,7 +68,7 @@ def get_arguments():
 
 if __name__ == "__main__":
     args = get_arguments()
-    client = mqtt.Client("VA Serving Frame Retrieval", userdata=args)
+    client = mqtt.Client("Pipeline Server Frame Retrieval", userdata=args)
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect(args.broker_address, args.broker_port)
