0da471aa636348355898701d51cdfda2b43290f6
Henry Bruce <henry.bruce@intel.com>
2022-02-22 12:21:45 -0800

    Remove tests

--- tests/util/start_kafka.sh
+++
@@ -1,3 +0,0 @@
-# Start simple Kafka broker
-echo "Launching zookeeper and kafka containers with auto-topic creation."
-docker-compose -f docker-compose-kafka-pipeline-server.yml up -d
--- tests/util/start_mqtt.sh
+++
@@ -1,1 +0,0 @@
-docker run -d --rm --name pipeline-server_mqtt -p 1883:1883 -p 9001:9001 eclipse-mosquitto
